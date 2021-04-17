e8f40f766915ccdc277a0b8440abe0ea2ee0bf5b
regycloud <regy888@gmail.com>
2021-04-17 22:16:30 +0700

    Adding debugging Errors and comment the errorExcercise file

---
+++ debuggingErrors.js
@@ -0,0 +1,18 @@
+// Before
+// function isSumBigger(number1, number2, total {
+//     const sum = number1 + number2;
+    // numberSum is not defined, subtitute with sum
+    // on the if parameter there is a semicolon that no need to be there. 
+//     if (numberSum > total;) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+  
+//   // Should return true
+//   console.log('isSumBigger(1, 3, 2) returns: ' + isSumBigger(1, 3, 2));
+  
+//   // Should return false
+//   console.log('isSumBigger(1, 3, 5) returns: ' + isSumBigger(1, 3, 5));
+
