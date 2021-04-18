506232916c3a2e982d3e3323bf4357102c49cb77
regycloud <regy888@gmail.com>
2021-04-18 22:01:31 +0700

    Finished for locatingSilentBugs

---
+++ locatingSilentBugs.js
@@ -0,0 +1,43 @@
+function capitalizeASingleWord(word) {
+
+  if (!word.match(' ')) {
+    
+    return null;
+  }
+  
+  let firstLetter = word.charAt(0);
+  const restOfWord = word.slice(1);
+  
+  firstLetter = firstLetter.toUpperCase();
+  
+  return firstLetter + restOfWord;
+}
+
+// Should return "Hey"
+console.log("capitalizeASingleWord('hey') returns: " + capitalizeASingleWord('hey')); 
+
+// Should return null
+console.log("capitalizeASingleWord('hey ho') returns: " + capitalizeASingleWord('hey ho'));
+
+// The solution: 
+
+function capitalizeASingleWord(word) {
+
+  if (word.match(' ')) {
+    console.log('Word value inside of if statement: ' + word);
+    return null;
+  }
+  
+  let firstLetter = word.charAt(0);
+  const restOfWord = word.slice(1);
+  
+  firstLetter = firstLetter.toUpperCase();
+  
+  return firstLetter + restOfWord;
+}
+
+// Should return "Hey"
+console.log("capitalizeASingleWord('hey') returns: " + capitalizeASingleWord('hey')); 
+
+// Should return null
+console.log("capitalizeASingleWord('hey ho') returns: " + capitalizeASingleWord('hey ho'));
