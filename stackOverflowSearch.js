e7aaf02ce8d80e521a93bb47b3f3c9dbab0f5cdd
regycloud <regy888@gmail.com>
2021-04-18 22:22:18 +0700

    Debugging by searching in Stackoverflow

---
+++ stackOverflowSearch.js
@@ -0,0 +1,12 @@
+
+// https://stackoverflow.com/questions/1789945/how-to-check-whether-a-string-contains-a-substring-in-javascript
+// Returns whether or not the provided string contains a substring of "cake" in it.
+function containsCake(string) {
+    return string.includes('cake');
+  }
+  
+  // Should return true
+  console.log("containsCake('I think cake is my soul mate.') returns: " + containsCake('I think cake is my soul mate.'));
+  
+  // Should return false
+  console.log("containsCake('Pie is certainly the coolest dessert.') returns: " + containsCake('Pie is certainly the coolest dessert.'));
